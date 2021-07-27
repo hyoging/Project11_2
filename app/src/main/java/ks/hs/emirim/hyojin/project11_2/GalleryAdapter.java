616eeb60657bea8eaf6f71cb16f8ef62c17d1b1b
User <w2030@e-mirim.hs.kr>
2021-07-27 11:52:58 +0900

    Edit toast1 Commit

--- app/src/main/java/ks/hs/emirim/hyojin/project11_2/GalleryAdapter.java
+++ app/src/main/java/ks/hs/emirim/hyojin/project11_2/GalleryAdapter.java
@@ -8,14 +8,20 @@ import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalleryAdapter extends BaseAdapter {
     Context context;
     int[] posterIds = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5,
             R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10};
+    int[] titleIds = {R.string.poster_title1, R.string.poster_title2, R.string.poster_title3, R.string.poster_title4, R.string.poster_title5,
+            R.string.poster_title6, R.string.poster_title7, R.string.poster_title8, R.string.poster_title9, R.string.poster_title10};
+    ImageView imgvLarge;
 
-    public GalleryAdapter(Context context){
+    public GalleryAdapter(Context context, ImageView imgvLarge){
         this.context = context;
+        this.imgvLarge = imgvLarge;
     }
     @Override
     public int getCount() {
@@ -38,6 +44,19 @@ public class GalleryAdapter extends BaseAdapter {
         imgv.setLayoutParams(new ViewGroup.LayoutParams(200,300));
         imgv.setImageResource(posterIds[position]);
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        return null;
+        final int pos = position;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgvLarge.setImageResource(posterIds[pos]);
+                Toast toast = new Toast(context);
+                View toastView = View.inflate(context, R.layout.toast1, null);
+                TextView texTile = toastView.findViewById(R.id.text_title);
+                texTile.setText(titleIds[pos]);
+                toast.setView(toastView);
+                toast.show();
+            }
+        });
+        return imgv;
     }
 }
